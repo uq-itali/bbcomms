58c080318db42612358a78946d729683cf38c822
Tim Scholl <t.scholl@uq.edu.au>
2022-03-17 08:15:51 +1000

    Change let to var

--- optionChanges.js
+++ optionChanges.js
@@ -1,26 +1,25 @@
-let nameInput = document.getElementById('ccName');
-let nameInputBox = document.getElementById('nameInputBox');
-    
-    var examPeriod = $('#examPeriodSelect').val();
-	     $('#examPeriodSelect').on('change', function(){
-		     if(examPeriod == "midSem"){
-			     $('span#examPeriodSelected').html("<strong>Mid-Semester</strong>")
-		     }
-		     else if(examPeriod == "endOfSem"){
-			     $('span#examPeriodSelected').html("<strong>End-of-Semester</strong>")
-		     }
-		     else if(examPeriod == ""){
-			     $('span#examPeriodSelected').html("<strong>Choose your semester above</strong>")
-		     }
-	     });
-         nameInputBox.addEventListener('input', function getText() {
-            nameInput.innerText = nameInputBox.value;
-            });
-        $('body').change(function(){
-                if(examPeriod != "" && nameInputBox.value != ""){
-                        $('#copyTemplateBtn').removeClass('d-none');
-                      }
-                else{
-                    $('#copyTemplateBtn').addClass('d-none');
-                }
-            });
+var nameInput = document.getElementById('ccName');
+var nameInputBox = document.getElementById('nameInputBox');
+var examPeriod = $('#examPeriodSelect').val();
+$('#examPeriodSelect').on('change', function(){
+    if(examPeriod == "midSem"){
+        $('span#examPeriodSelected').html("<strong>Mid-Semester</strong>")
+    }
+    else if(examPeriod == "endOfSem"){
+        $('span#examPeriodSelected').html("<strong>End-of-Semester</strong>")
+    }
+    else if(examPeriod == ""){
+        $('span#examPeriodSelected').html("<strong>Choose your semester above</strong>")
+    }
+});
+nameInputBox.addEventListener('input', function getText() {
+    nameInput.innerText = nameInputBox.value;
+});
+$('body').change(function(){
+    if(examPeriod != "" && nameInputBox.value != ""){
+        $('#copyTemplateBtn').removeClass('d-none');
+    }
+    else{
+        $('#copyTemplateBtn').addClass('d-none');
+    }
+});
