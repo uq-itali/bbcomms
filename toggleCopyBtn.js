36ce77a898fb0e29abd57f04decd9a306786343a
Tim Scholl <t.scholl@uq.edu.au>
2022-03-16 21:41:04 +1000

    update external JS

--- toggleCopyBtn.js
+++ toggleCopyBtn.js
@@ -1,4 +1,16 @@
 window.addEventListener("load", function(){
+    var examPeriod = $('#examPeriodSelect').val();
+	     $('#examPeriodSelect').on('change', function(){
+		     if(examPeriod == "midSem"){
+			     $('span#examPeriodSelected').html("<strong>Mid-Semester</strong>")
+		     }
+		     else if(examPeriod == "endOfSem"){
+			     $('span#examPeriodSelected').html("<strong>End-of-Semester</strong>")
+		     }
+		     else if(examPeriod == ""){
+			     $('span#examPeriodSelected').html("<strong>Choose your semester above</strong>")
+		     }
+	     });
         $('body').change(function(){
                 if(examPeriod != "" && nameInputBox.value != ""){
                         $('#copyTemplateBtn').removeClass('d-none');
