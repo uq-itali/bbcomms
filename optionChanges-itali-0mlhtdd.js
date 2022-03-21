353e7dc421a8de45bb249d36b0bf1d103e50ca35
Tim Scholl <t.scholl@uq.edu.au>
2022-03-21 15:38:12 +1000

    updating

---
+++ optionChanges-itali-0mlhtdd.js
@@ -0,0 +1,37 @@
+window.addEventListener('load',function(){
+    let nameInput = document.getElementById('ccName');
+    let nameInputBox = document.getElementById('nameInputBox');
+    
+    $('#date1').change(function(){
+        var dateSelected = document.getElementById('date1').value
+        var dateSelectedTest = document.getElementById('dateSelected1')
+        dateSelectedTest.innerText = dateSelected.value
+    })
+    $('#examPeriodSelect').change(function(){
+    var examPeriod = document.getElementById('examPeriodSelect').value;
+        if(examPeriod == 'midSem'){
+            $('span#examPeriodSelected').html("<strong>Mid-Semester</strong>")
+        }
+        else if(examPeriod == 'endOfSem'){
+            $('span#examPeriodSelected').html("<strong>End-of-Semester</strong>")
+        }
+        else if(examPeriod == ''){
+            $('span#examPeriodSelected').html("<strong>Choose your semester above</strong>");
+        }
+    });
+    nameInputBox.addEventListener('input', function getText(){
+        nameInput.innerText = nameInputBox.value;
+    });
+    $('body').change(function(){
+        var examPeriod = document.getElementById('examPeriodSelect').value;
+        if(examPeriod != "" && nameInputBox.value != ""){
+            $('#copyTemplateBtn').removeClass('d-none');
+        }
+        else{
+            $('#copyTemplateBtn').addClass('d-none');
+            alert('Please ensure you have completed all the necessary fields');
+            $('#examPeriodSelect').toggleClass('border-danger');
+            $('#nameInputBox').toggleClass('');
+        }
+    });
+});
