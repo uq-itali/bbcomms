1c76e7a865f98d072a60f2dac277be6f90a03708
Tim Scholl <t.scholl@uq.edu.au>
2022-03-26 22:44:31 +1000

    add options plus inject complex regex variables

--- optionChanges.js
+++ optionChanges.js
@@ -1,19 +1,41 @@
 window.addEventListener('load', function() {
-    //  let nameOutput = document.getElementsByClassName('ccName');
+    $('input[name="assessmentTypes"]').on('click', function() {
+            var assessmentTypesArray = []
+            var assessmentTypesChecked = document.querySelectorAll('input[name="assessmentTypes"]:checked')
+            for (var i = 0; i < assessmentTypesChecked.length; i++) {
+                assessmentTypesArray.push(assessmentTypesChecked[i].value)
+            }
+            var assessmentTypeArrayToString = assessmentTypesArray.toString().replaceAll(',', ', ');
+            console.log(assessmentTypeArrayToString);
+            if (assessmentTypeArrayToString.indexOf("Exam") >= 0) {
+                var assessmentTypeArrayFinalList = assessmentTypeArrayToString.replace(/,(?=[^,]+$)/, ' and an')
+                var assessmentTypeListOr = assessmentTypeArrayToString.replace(/,(?=[^,]+$)/, ' or an')
+            } else {
+                var assessmentTypeArrayFinalList = assessmentTypeArrayToString.replace(/,(?=[^,]+$)/, ' and')
+                var assessmentTypeListOr = assessmentTypeArrayToString.replace(/,(?=[^,]+$)/, ' or')
+            }
+            console.log(assessmentTypeListOr);
+            console.log(assessmentTypeArrayFinalList);
+            $('.assessmentTypeListOr').text(assessmentTypeListOr)
+            $('.assessmentTypesSelected').text(assessmentTypeArrayFinalList)
+        })
+        //  let nameOutput = document.getElementsByClassName('ccName');
     let nameInputBox = document.getElementById('nameInputBox');
     //  let courseCodeOutput = document.getElementById('courseCodeOutput');
     let courseCodeInputBox = document.getElementById('courseCode');
     //let courseTitleOutput = document.getElementById('courseTitleOutput');
     let courseTitleInputBox = document.getElementById('courseTitle');
-    let examSelected = document.getElementById('exam')
+    let bbAssessmentAreaLinkInputBox = document.getElementById('bbAssessmentAreaLink')
+    let examSelected = document.getElementById('exam');
+    var examPeriodSelected = $("input:radio[name='examPeriod']:checked").val();
+    var examPeriodSelected = $("input:checkbox[name='assessmentTypes']:checked").val();
     $('#examPeriodSelect').change(function() {
-        var examPeriod = document.getElementById('examPeriodSelect').value;
-        if (examPeriod == 'midSem') {
+        if (examPeriodSelected == 'midSem') {
             $('span#examPeriodSelected').html("<strong>Mid-Semester</strong>")
         } else if (examPeriod == 'endOfSem') {
             $('span#examPeriodSelected').html("<strong>End-of-Semester</strong>")
-        } else if (examPeriod == '') {
-            $('span#examPeriodSelected').html("<strong>Choose your semester above</strong>");
+        } else if (examPeriod == 'bothExamPeriods') {
+            $('span#examPeriodSelected').html("<strong>Mid-Semester and End-of-Semester</strong>");
         }
     });
     nameInputBox.addEventListener('input', function getCCName() {
@@ -25,6 +47,9 @@ window.addEventListener('load', function() {
     courseTitleInputBox.addEventListener('input', function getCourseTitle() {
         $('.courseTitleOutput').text(courseTitleInputBox.value)
     });
+    bbAssessmentAreaLinkInputBox.addEventListener('input', function getCourseTitle() {
+        $('.bbAssessmentArea').attr('href', bbAssessmentAreaLinkInputBox.value)
+    });
     $(examSelected).change(function() {
         if (examSelected.checked == true) {
             $('.ifExamSelected').removeClass('d-none');
@@ -34,7 +59,6 @@ window.addEventListener('load', function() {
     });
     $('body').change(function() {
         var selectedDate = linked1.viewDate;
-        var examPeriod = document.getElementById('examPeriodSelect').value;
         if (courseCodeInputBox.value != "" && courseTitleInputBox.value != "" && nameInputBox.value != "" && selectedDate != "") {
             $('#copyTemplateBtn, #ecpSuggestedTextCard, #initialBBCommsTextCard').removeClass('d-none');
             //$('#ecpSuggestedTextCard').removeClass('d-none');
@@ -42,12 +66,12 @@ window.addEventListener('load', function() {
             $('#copyTemplateBtn, #ecpSuggestedTextCard, #initialBBCommsTextCard').addClass('d-none');
         }
         /*
-       else{
-            $('#copyTemplateBtn').addClass('d-none');
-            alert('Please ensure you have completed all the necessary fields');
-            $('#examPeriodSelect').toggleClass('border-danger');
-            $('#nameInputBox').toggleClass('');
-        }*/
+           else{
+                $('#copyTemplateBtn').addClass('d-none');
+                alert('Please ensure you have completed all the necessary fields');
+                $('#examPeriodSelect').toggleClass('border-danger');
+                $('#nameInputBox').toggleClass('');
+            }*/
 
     });
     $('#date1, #date2').click(function() {
